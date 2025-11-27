742b74cbf3851255d6e1fd7f29605ffd64c36127
Gernet, Marco <marco.gernet@study.thws.de>
2025-11-27 17:59:09 +0100

    Example 8 – Introduction to MenuBars

---
+++ beispiel_8/beispiel_8.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        mb = self.menuBar()
+        fm_1 = mb.addMenu("File")
+        fm_2 = mb.addMenu("Edit")
+
+        actionSchliessen = QAction("Close Window", self)
+        actionSchliessen.triggered.connect(self.close)
+        actionTest = QAction("Open File", self)
+        actionTest.triggered.connect(self.close)
+        fm_1.addAction(actionSchliessen)
+        fm_1.addAction(actionTest)
+
+        self.resize(800, 600)
+        self.setWindowTitle("Beispiel 7")
+        self.show()
+
+
+
+app = QApplication([])
+window = MainWindow()
+app.exec_()
